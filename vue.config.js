dce17bb84813abbd84e830bb94b793f10268d4b2
mizu <psdk55052004@gmail.com>
2022-07-05 01:59:29 +0800

    修改 設定檔路徑修改

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Vue-CLI-Template/' //* 儲存庫名稱
+    ? '/Chart.js-Practice/' //* 儲存庫名稱
     : '/'
 })
